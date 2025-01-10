86f2796155b245f74f0ffa16346277c3d66a41b5
Philip Røst Wehinger <philip.r.wehinger@gmail.com>
2025-01-10 12:16:09 +0100

    thumbs up and down with button and state

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+THUMBS_DOWN = "tommel_ned.png"
+THUMBS_UP = "tommel_opp.png"
+st.title("Is the call center APP running?")
+
+
+# Initialize session state for the toggle
+if "show_thumbs_up" not in st.session_state:
+    st.session_state.show_thumbs_up = True
+
+# Function to toggle the state
+def toggle_image():
+    st.session_state.show_thumbs_up = not st.session_state.show_thumbs_up
+
+# Display the appropriate image based on state
+if st.session_state.show_thumbs_up:
+    st.image(THUMBS_UP, caption="Thumbs Up", use_container_width=True)
+else:
+    st.image(THUMBS_DOWN, caption="Thumbs Down", use_container_width =True)
+
+# Button to toggle the state
+st.button("Change Status", on_click=toggle_image)
